68de05f2a82d8acbeae9d2e97f0634f8d048742a
Marie-Carrere <marie.carrere@icloud.com>
2020-04-13 08:20:31 +0200

    :tada: Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,13 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>Learn to design and code React app</h1>
+    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
