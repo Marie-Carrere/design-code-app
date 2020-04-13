1395ae68534f49dd43840ab6f6d9246ee0a8d2cc
Marie-Carrere <marie.carrere@icloud.com>
2020-04-13 09:41:53 +0200

    :lipstick: Add images and style Hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Learn to design and code React app</h1>
-    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code modern applications</h1>
+        <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift</p>
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div>
   </Layout>
 )
 
