38bb5a0327f4ba6f82af2568527c9fa853649f9f
Marie-Carrere <marie.carrere@icloud.com>
2020-04-15 16:19:22 +0200

    :sparkles: Add hooks to handle different menu when scrolling the page

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,40 @@
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
 import "../styles/Header.css"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <div className="Header">
-      <div className="HeaderGroup">
-        <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" alt=""/></Link>
-        <Link to="/courses">Courses</Link>
-        <Link to="/download">Downloads</Link>
-        <Link to="/workshops">Workshops</Link>
-        <Link to="/buy"><button>Buy</button></Link>
+const Header = ({ siteTitle }) => {
+  const [scrolled, setScrolled] = useState(false)
+  
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+  
+  const handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+    
+    if (scrollTop > 50) {
+      setScrolled(true)
+    } else {
+      setScrolled(false)
+    }
+  }
+  
+  return(
+    <header>
+      <div className={scrolled ? 'Header HeaderScrolled' : 'Header'} >
+        <div className="HeaderGroup">
+          <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" alt="" /></Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/download">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/buy"><button>Buy</button></Link>
+        </div>
       </div>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
